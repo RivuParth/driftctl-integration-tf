2ce17b3d0b1ef12baaf52b81993f14b002924dc9
RivuParth <bhowmick.partha1999@gmail.com>
2024-10-02 21:32:18 +0530

    added aws cred ttfffl

--- variables.tf
+++ variables.tf
@@ -10,3 +10,9 @@ variable "bucket_name" {
   description = "Name of the S3 bucket"
   default     = "my-sample-bucket"
 }
+
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1" # Change this to your preferred region
+}
