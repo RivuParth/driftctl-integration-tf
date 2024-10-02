2fb39693721927b7ae8ec056409173499857a2c8
RivuParth <bhowmick.partha1999@gmail.com>
2024-10-02 21:47:19 +0530

    awsffpyiii  j jjjoo

--- variables.tf
+++ variables.tf
@@ -14,5 +14,5 @@ variable "bucket_name" {
 variable "aws_region" {
   description = "The AWS region to deploy resources"
   type        = string
-  default     = "us-west-2"  # Set your preferred region
+  default     = "us-east-1"  # Set your preferred region
 }
